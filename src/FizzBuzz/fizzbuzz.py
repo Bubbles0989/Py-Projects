38e59811cb3e98358db538d2ef214b6164957ce5
Bubbles0989 <jasmith17@mail.wou.edu>
2023-08-28 15:14:56 -0700

    finished fizzbuzz solution and tests

--- src/FizzBuzz/fizzbuzz.py
+++ src/FizzBuzz/fizzbuzz.py
@@ -1,12 +1,30 @@
 
 
 def fizzbuzz(x):
-    pass
+    if x > 100:
+        return 'Numbers between 1 and 100 please.'
+    elif x < 1:
+        return 'Numbers between 1 and 100 please.'
+    
+    remainder_of_3 = x % 3
+    remainder_of_5 = x % 5
+
+    if remainder_of_3 == 0 and remainder_of_5 == 0:
+        return 'fizzbuzz'
+    elif remainder_of_5 == 0:
+        return 'buzz'
+    elif remainder_of_3 == 0:
+        return 'fizz'
+    else:
+        return x
 
 
 if __name__ == "__main__":
-    print("Fizzbuzz~ <3")
+    print("Fizzbuzz~ <3\n")
 
     userInput = int(input("Give me a number between 1 and 100 please~ >> "))
 
-    fizzbuzz(userInput)
+    print(fizzbuzz(userInput))
+
+    # for number in range(0, 101):
+    #     print(fizzbuzz(number))   
--- src/FizzBuzz/test_fizzbuzz.py
+++ src/FizzBuzz/test_fizzbuzz.py
@@ -1,5 +1,8 @@
 from fizzbuzz import fizzbuzz
 
+def test_fizzbuzz_2_returns_2():
+    assert fizzbuzz(2) == 2
+
 def test_fizzbuzz_21_returns_fizz():
     assert fizzbuzz(21) == 'fizz'
 
@@ -13,4 +16,4 @@ def test_fizzbuzz_negative_returns_informational_message():
     assert fizzbuzz(-10) == 'Numbers between 1 and 100 please.'
 
 def test_fizzbuzz_over_100_returns_informational_message():
-    assert fizzbuzz(100) == 'Numbers between 1 and 100 please.'
+    assert fizzbuzz(101) == 'Numbers between 1 and 100 please.'
