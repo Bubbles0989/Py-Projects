9d3b64370b8d7b2cb5d4d9e7c8a714ceb61cbec4
Bubbles0989 <jasmith17@mail.wou.edu>
2023-08-31 16:02:05 -0700

    fixed bowling logic, readjusted tests and got pycoverage goin

--- src/Bowling/bowling.py
+++ src/Bowling/bowling.py
@@ -5,44 +5,89 @@ class bowling:
     def __init__(self):
         self.frames = []
         self.current_frame = []
-        self.frame_count = 1
+        self.frame_count = 0
 
     def roll(self, pins):
-        if pins == 10 and len(self.current_frame) == 1:
+        if len(self.current_frame) == 2 and self.frame_count != 9:
+            self.frames.append(self.current_frame)
+            self.frame_count += 1
+            self.current_frame = []    
+        elif len(self.current_frame) == 2 and self.frame_count == 9:
+            if pins == 10:
+                self.current_frame.append('X')
+                self.frames.append(self.current_frame)
+                self.frame_count += 1
+                return    
+            elif self.current_frame[1] != 'X' and self.current_frame[1] + pins == 10:
+                self.current_frame.append('/')
+                self.frames.append(self.current_frame)
+                self.frame_count += 1
+                return
+            else:
+                self.current_frame.append(pins)
+                self.frames.append(self.current_frame)
+                self.frame_count += 1
+                return
+
+        if pins == 10 and len(self.current_frame) == 1 and self.frame_count != 9:
             self.current_frame = [0, '/']
             self.frames.append(self.current_frame)
             self.frame_count += 1
             self.current_frame = []
-        elif pins == 10:
+        elif pins == 10 and self.frame_count != 9:
             self.current_frame = ['X']
             self.frames.append(self.current_frame)
             self.frame_count += 1
             self.current_frame = []
-        elif len(self.current_frame) == 2 and self.frame_count != 9:
-            self.frames.append(self.current_frame)
-            self.frame_count += 1
-            self.current_frame = []
-            self.current_frame.append(pins)
-        elif len(self.current_frame) == 2 and self.frame_count == 9:
-            self.current_frame.append(pins)
-            self.frames.append(self.current_frame)
-            self.frame_count += 1
-        elif len(self.current_frame) == 1 and self.current_frame[0] + pins > 10:
+        elif pins == 10 and self.frame_count == 9:
+            self.current_frame.append('X')
+        elif len(self.current_frame) == 1 and self.frame_count != 9 and self.current_frame[0] + pins > 10:
             raise ValueError("There can only be ten pins in a single frame.....")
-        elif len(self.current_frame) == 1 and self.current_frame[0] + pins == 10:
+        elif len(self.current_frame) == 1 and self.frame_count != 9 and self.current_frame[0] + pins == 10:
             self.current_frame = [self.current_frame[0], '/']
             self.frames.append(self.current_frame)
             self.frame_count += 1
             self.current_frame = []
+        elif len(self.current_frame) == 1  and self.frame_count == 9 and self.current_frame[0] != 'X' and self.current_frame[0] + pins == 10:
+            self.current_frame.append('/')
         else:
             self.current_frame.append(pins)
-
+ 
     def score(self):
-        pass
-
-    def play(self):
         pass
 
 
 if __name__ == "__main__":
-    print("Bowl!")
+    test_game = bowling()
+    # test_game.roll(5)
+    # test_game.roll(3)
+
+    # test_game.roll(2)
+    # test_game.roll(4)
+
+    # test_game.roll(0)
+    # test_game.roll(10)
+
+    # test_game.roll(4)
+    # test_game.roll(6)
+
+    # test_game.roll(7)
+    # test_game.roll(2)
+
+    # test_game.roll(3)
+    # test_game.roll(2)
+
+    # test_game.roll(5)
+    # test_game.roll(3)
+    
+    # test_game.roll(1)
+    # test_game.roll(2)
+
+    # test_game.roll(4)
+    # test_game.roll(4)
+    
+    # test_game.roll(10)
+    # test_game.roll(10)
+    # test_game.roll(10)
+    # print(test_game.frames)
+
--- src/Bowling/test_bowling.py
+++ src/Bowling/test_bowling.py
@@ -1,15 +1,40 @@
 from bowling import bowling
 import pytest
-import unittest
 
 @pytest.fixture(scope="session", autouse=True)
 def test_bowl_with_frames():
     test_game = bowling()
     test_game.roll(5)
     test_game.roll(3)
+
+    test_game.roll(2)
     test_game.roll(4)
+
+    test_game.roll(0)
+    test_game.roll(10)
+
+    test_game.roll(4)
+    test_game.roll(6)
+
+    test_game.roll(7)
+    test_game.roll(2)
+
+    test_game.roll(3)
+    test_game.roll(2)
+
+    test_game.roll(5)
+    test_game.roll(3)
+    
+    test_game.roll(1)
+    test_game.roll(2)
+
+    test_game.roll(4)
+    test_game.roll(4)
+    
+    test_game.roll(5)
     test_game.roll(5)
     test_game.roll(10)
+
     return test_game
 
 def test_roll_appends_full_frame_to_frames(test_bowl_with_frames):
@@ -43,10 +68,22 @@ def test_roll_incorrect_frame_numbers():
     test_game.roll(6)
     assert ValueError, test_game.roll(6)
 
-def test_tenth_frame():
+def test_tenth_frame_with_x(test_bowl_with_frames):
+    test_game = test_bowl_with_frames
+    assert test_game.frames[9][2] == 'X'
+
+def test_tenth_frame_spare(test_bowl_with_frames):
+    test_game = test_bowl_with_frames
+    assert test_game.frames[9][1] == '/'
+
+def test_tenth_frame(test_bowl_with_frames):
+    test_game = test_bowl_with_frames
+    assert test_game.current_frame == [5, '/', 'X']
+
+def test_tenth_frame_last_pin_spare():
     test_game = bowling()
     test_game.roll(5)
-    test_game.roll(5)
+    test_game.roll(3)
 
     test_game.roll(2)
     test_game.roll(4)
@@ -72,10 +109,45 @@ def test_tenth_frame():
     test_game.roll(4)
     test_game.roll(4)
     
+    test_game.roll(10)
+    test_game.roll(7)
+    test_game.roll(3)
+
+    assert test_game.current_frame == ['X', 7, '/']
+
+def test_tenth_frame_without_strikes():
+    test_game = bowling()
     test_game.roll(5)
     test_game.roll(3)
-    test_game.roll(1)
 
-    assert test_game.current_frame == [5, 3, 1]
+    test_game.roll(2)
+    test_game.roll(4)
+
+    test_game.roll(0)
+    test_game.roll(10)
+
+    test_game.roll(4)
+    test_game.roll(6)
+
+    test_game.roll(7)
+    test_game.roll(2)
+
+    test_game.roll(3)
+    test_game.roll(2)
+
+    test_game.roll(5)
+    test_game.roll(3)
+    
+    test_game.roll(1)
+    test_game.roll(2)
+
+    test_game.roll(4)
+    test_game.roll(4)
+    
+    test_game.roll(10)
+    test_game.roll(3)
+    test_game.roll(2)
+
+    assert test_game.current_frame == ['X', 3, 2]
 
 
