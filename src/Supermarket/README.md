0ea89ed0c95d07cf99e19e28c64046b1691e097f
Bubbles0989 <jasmith17@mail.wou.edu>
2023-10-16 17:34:49 -0700

    redid css and started cart server communication

--- src/Supermarket/README.md
+++ src/Supermarket/README.md
@@ -3,3 +3,4 @@
 Write a program that simulates a supermarket with prices, sales, a cart, etc.. 
 
 > I ended up making this into a cute little flask python app
+> ChatGPT was used as an assitant in this project
--- src/Supermarket/supermarket.py
+++ src/Supermarket/supermarket.py
@@ -2,6 +2,7 @@ import datetime
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask import flash, redirect, render_template, request, url_for
+from config import SECRET_KEY
 
 def create_app():
     app = Flask(__name__)
@@ -9,34 +10,26 @@ def create_app():
     return app
 
 app = create_app()
-cart = ""
+app.secret_key = SECRET_KEY
+cart_items = []
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
     shop_items = { 'Cheese':4.99, 'Bread':6.99, 'Soup':3.99, 'Water':8.99 }
 
-    return render_template(
-        "index.html",
-        greeting_name="Guest",
-        datetime_object=datetime.datetime.utcnow(),
-        shop_items=shop_items
-    )
+    if request.method == 'POST':
+        item = request.form.get('cart-button')
+        split_items = item.split(',')
+        for string in split_items:
+            cart_items.append(string)
 
-@app.route("/addCart/", methods=['GET'])
-def add_to_cart():
-    # cart = request.form["cart-button"]
-    pass
+        flash(f'{split_items[0]} added to cart!', 'success')
+
+    return render_template("index.html", shop_items=shop_items)
 
 @app.route("/cart")
 def cart():
-    cart_items = cart
-
-    return render_template(
-        "cart.html",
-        greeting_name="Guest",
-        datetime_object=datetime.datetime.utcnow(),
-        cart_items=cart_items
-    )
+    return render_template("cart.html", cart_items=cart_items)
 
 @app.route("/about")
 def about():
